4cebfd1644177073a323a0d44bdfb0c210013816
tdch <tdch@ymail.com>
2010-06-25 08:14:25 +0000

    User signup complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,14 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
 
 # Uncomment the next line to use webrat's matchers
-#require 'webrat/integrations/rspec-rails'
+require 'webrat/integrations/rspec-rails'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
